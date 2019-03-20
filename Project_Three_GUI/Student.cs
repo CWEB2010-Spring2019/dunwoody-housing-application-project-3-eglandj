31491105be304fe0b971c0c490749491a13255aa
Egle <eglandj@dunwoody.edu>
2019-03-20 12:05:22 -0500

    forgot to add student class

---
+++ Project_Three_GUI/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Three_GUI
+{
+    abstract class Student
+    {
+        protected int ID_Number;
+        protected string firstName;
+        protected string lastName;
+        protected int floorNumber;
+        protected int roomNumber;
+        protected double rentFee;
+    }
+    class Scholarship : Student
+    {
+        public static int[] floorOptions = { 1, 2, 3 };
+        public Scholarship(int id, string fname, string lname, int floor, int room)
+        {
+            ID_Number = id;
+            firstName = fname;
+            lastName = lname;
+            floorNumber = floor;
+            roomNumber = room;
+            rentFee = 100;
+        }
+    }
+    class Athlete : Student
+    {
+        public static int[] floorOptions = { 4, 5, 6 };
+        public Athlete(int id, string fname, string lname, int floor, int room)
+        {
+            ID_Number = id;
+            firstName = fname;
+            lastName = lname;
+            floorNumber = floor;
+            roomNumber = room;
+            rentFee = 1200;
+        }
+    }
+    class Worker : Student
+    {
+        public static int[] floorOptions = { 7, 8 };
+        private double hourlyPay;
+        private double monthlyHours;
+        public Worker(int id, string fname, string lname, int floor, int room, int monthHours)
+        {
+            ID_Number = id;
+            firstName = fname;
+            lastName = lname;
+            floorNumber = floor;
+            roomNumber = room;
+            monthlyHours = monthHours;
+            hourlyPay = 14.00;
+            double monthlyPay = hourlyPay * monthlyHours;
+            rentFee = 1245 - (0.5 * monthlyPay);
+        }
+    }
+}
